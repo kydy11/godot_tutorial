b9056561663276336ad45eb32491d8d5071b45c2
Merit Student <student@meac.local>
2019-09-09 09:47:58 -0600

    Add files

---
+++ icon2.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+const METERS =300;
+const GRAVITY =9.8;
+
+var moveSpeed =1;
+var jumpHight =4;
+var jumpTime =1;
+var currantJumpTime =0;
+var hasJumbed =false;
+
+var velocity = Vector2()
+
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("left"):
+		velocity.x = -moveSpeed *METERS;
+	elif Input.is_action_pressed("right"):
+		velocity.x = moveSpeed *METERS;
+	else:
+		velocity.x =0;
+	
+	if Input.is_action_just_pressed("up") && velocity.y==0:
+		currantJumpTime=jumpTime;
+	
+	if currantJumpTime>0:
+		velocity.y -= (jumpHight/jumpTime) *currantJumpTime *METERS;
+		currantJumpTime-=1;
+	
+	
+	velocity.y += delta * GRAVITY * METERS;
+	
+	if move_and_slide(velocity)[1]==0:
+		velocity.y =0;
